eae99f3ad12be0c4b2c658f7fad14feebcd4e66b
noreen <na429@student.le.ac.uk>
2025-07-24 01:48:56 +0300

    edited

--- memoryCardGame.js
+++ memoryCardGame.js
@@ -4,7 +4,7 @@ let firstCard, secondCard;
 let lockBoard = false;
 let score = 0;
 document.querySelector(".score").textContent = score;
-fetch("./data/data.json")
+fetch("/data/data.json")
     .then((res) => res.json())
     .then((data) => {
         cards = [...data, ...data];
